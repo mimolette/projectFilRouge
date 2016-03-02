8fca19481dbc9cf5e73eaf969c8d99b5cbd73826
olivier cremieux <olivier.dl12@formation.imie>
2016-03-02 11:14:44 +0100

    ajout class episode

---
+++ src/SerieBundle/Entity/Episode.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace SerieBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Episode
+ *
+ * @ORM\Table(name="episode")
+ * @ORM\Entity(repositoryClass="SerieBundle\Repository\EpisodeRepository")
+ */
+class Episode
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="num", type="integer")
+     */
+    private $num;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="releaseDate", type="date")
+     */
+    private $releaseDate;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set num
+     *
+     * @param integer $num
+     * @return Episode
+     */
+    public function setNum($num)
+    {
+        $this->num = $num;
+
+        return $this;
+    }
+
+    /**
+     * Get num
+     *
+     * @return integer 
+     */
+    public function getNum()
+    {
+        return $this->num;
+    }
+
+    /**
+     * Set releaseDate
+     *
+     * @param \DateTime $releaseDate
+     * @return Episode
+     */
+    public function setReleaseDate($releaseDate)
+    {
+        $this->releaseDate = $releaseDate;
+
+        return $this;
+    }
+
+    /**
+     * Get releaseDate
+     *
+     * @return \DateTime 
+     */
+    public function getReleaseDate()
+    {
+        return $this->releaseDate;
+    }
+}
