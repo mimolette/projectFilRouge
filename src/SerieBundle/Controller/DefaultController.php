22bf0c9f59d6c658b0f4e3278197319ef7d8497e
Metivier Anthony <anthony-met@hotmail.fr>
2016-03-08 14:21:01 +0100

    ajout top10

--- src/SerieBundle/Controller/DefaultController.php
+++ src/SerieBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace SerieBundle\Controller;
 
+use SerieBundle\Entity\Serie;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
@@ -14,5 +15,32 @@ class DefaultController extends Controller
     public function top10Action()
     {
         return $this->render('SerieBundle:Default:top10.html.twig');
+    }
+
+    public function detailAction($id)
+    {
+        $serie = $this
+            ->getDoctrine()
+            ->getRepository("SerieBundle:Serie")
+            ->find($id);
+
+        return $this->render('SerieBundle:Default:detail.html.twig', ["serie" => $serie]);
+    }
+
+    public function delAction($id)
+    {
+        $serie = $this
+            ->getDoctrine()
+            ->getRepository("SerieBundle:Serie")
+            ->find($id);
+
+        $em = $this->getDoctrine()->getManager();
+        if(null === $serie){
+            throw $this->createNotFoundException();
+        }
+        $em->remove($serie);
+        $em->flush();
+
+        return $this->redirectToRoute('serie_homepage');
     }
 }
